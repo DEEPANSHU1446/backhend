b284c96f7700d882433c953487ba9ff262fb8e6d
DEEPANSHU1446 <yadavdeepanshu1446@gmail.com>
2022-08-08 23:30:38 +0530

    complete

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,43 @@
 const express = require('express');
-const newi = require('../logger/logger')
-const new2 = require('../util/helper')
-const new3 = require('../validator/formatter')
+// const newi = require('../logger/logger')
+// const new2 = require('../util/helper')
+// const new3 = require('../validator/formatter')
+const lodash=require('lodash')
 const router = express.Router();
 
 router.get('/test-me', function (req, res) {
-    newi.welcome()
-    new2.prdate()
-    new2.prmonth()
-    new2.prinform()
-    new3.trims()
-    new3.uppr()
-    new3.lowr()
-    res.send('My last function also running')
-});
+    // newi.welcome()
+    // new2.prdate()
+    // new2.prmonth()
+    // new2.prinform()
+    // new3.trims()
+    // new3.uppr()
+    // new3.lowr()
+    // router.get('/test-me', function (req, res) {
+    //     res.send('hii,i am Deepanshu yadav')
+        // no.1
+        const mon=["jan","feb","mar","apr","may","june","july","aug","sep","oct","nov","dec"]
+        console.log(lodash.chunk(mon, 3))
+    
+        // no.2
+    
+        const od=[1,3,5,7,9,11,13,15,17,19]
+        console.log(lodash.tail(od));
+    
+        //no.3
+    
+        const arr=[1,2,3,4,5]
+        console.log(lodash.union(arr,[2,6]));
+    
+        //no.4
+    
+        const obj =[ ["horror","The Shining"],["drama",'Titanic'],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]
+        console.log(lodash.fromPairs(obj));
+    });
+    
+    // res.send('My last function also running')
+
+
 router.get('/test-intro', function (req, res) {
     
 });
