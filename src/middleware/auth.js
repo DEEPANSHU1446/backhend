9bdac12c8ce8aa0394c73bc494c828b354849a7d
DEEPANSHU1446 <yadavdeepanshu1446@gmail.com>
2022-08-29 00:30:42 +0530

    token assgnmt

---
+++ src/middleware/auth.js
@@ -0,0 +1,7 @@
+const mid = function (req,res,next){
+ let token = req.headers["x-auth-token"];
+  if (!token) {return res.send({ status: false, msg: "token must be present" })
+} 
+else{ next()}
+}
+module.exports={mid}
